7d37a77ceabe74b1ddc9c5d750983460238b3c83
SergioBotero <sergiobuj+github@gmail.com>
2011-04-16 13:48:34 -0500

    zinergia on rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Zinergiaweb::Application.config.secret_token = 'c3527d736a3016d35b2f92fe6ef27321649736867c131629bc2de118d825625668cef25506135d997e6dea5bec789d4ec70360dbf92b99607c9675b46c17a070'
