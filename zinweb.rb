f0aaac75c6c6a4301186c2b99b97a3863a07cb92
Sebastian Arcila Valenzuela <sebastianarcila@gmail.com>
2011-04-19 18:09:41 -0500

    team pages

--- zinweb.rb
+++ zinweb.rb
@@ -1,5 +1,17 @@
 # zinweb.rb
+require 'erb'
+
+set :views, File.dirname(__FILE__) + '/views'
 
 get "/" do
   File.read(File.join('public', 'index.html'))
+end
+TEAM = ['sebastian', 'sergio', 'alejandro', 'juan', 'felipe', 'nicolas']
+get "/team/:name" do
+  name = params[:name]
+  if TEAM.include? name
+    erb "team/#{name}".to_sym
+  else
+    halt 404
+  end
 end
